b9f51f8d940868c7faf896c88100aa1b571da8ce
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2025-09-17 20:55:55 -0500

    feat: mercadolibre structure addition

---
+++ mercadolibre/urls.py
@@ -0,0 +1,14 @@
+from django.urls import re_path
+from mercadolibre.views import (
+    auth, products, orders, inventory
+)
+
+mercadolibre_endpoints = [
+    re_path(r'^auth/login$', auth.login),
+    re_path(r'^auth/refresh$', auth.refresh_token),
+    re_path(r'^products/sync$', products.sync),
+    re_path(r'^products/update$', products.update),
+    re_path(r'^orders/sync$', orders.sync),
+    re_path(r'^orders/update$', orders.update),
+    re_path(r'^inventory/update$', inventory.update),
+]
--- project/settings.py
+++ project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "wmsAdapterV2",
     "wmsBase",
+    "mercadolibre",
 ]
 
 MIDDLEWARE = [
