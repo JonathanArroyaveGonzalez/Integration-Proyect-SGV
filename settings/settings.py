81cb63012f757ce400f49113f80169c46d8cb027
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2025-11-07 23:50:47 -0500

    chore: add DATABASE_NAME configuration from environment variables

--- settings/settings.py
+++ settings/settings.py
@@ -8,5 +8,6 @@ class global_settings:
 
     try:
         CONNECTION_STRING = os.getenv("DATABASE_URL")
+        DATABASE_NAME = os.getenv("DBNAMEWMS")
     except KeyError as e:
         raise RuntimeError("Could not find a CONNECTION_STRING in environment") from e
